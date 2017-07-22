147b76a884d8519c3334ba1e0526e4d650a40c01
José Quiñones Castro <josequicas@gmail.com>
2017-07-22 02:47:49 +0200

    Skybox added

--- src/app/shared/services/render.service.ts
+++ src/app/shared/services/render.service.ts
@@ -4,6 +4,15 @@ import * as BABYLON from 'babylonjs';
 @Injectable()
 export class RenderService {
 
+  private SKYBOX_EXTENSIONS: string[] = [
+    '_px.png',
+    '_py.png',
+    '_pz.png',
+    '_nx.png',
+    '_ny.png',
+    '_nz.png'
+  ];
+
   constructor() { }
 
   public renderBoard(canvas: HTMLCanvasElement) {
@@ -28,23 +37,18 @@ export class RenderService {
     // Now create a basic Babylon Scene object
     const scene = new BABYLON.Scene(engine);
     // Change the scene background color to green.
-    scene.clearColor = new BABYLON.Color4(0, 1, 0, 1);
+    scene.clearColor = new BABYLON.Color4(0, 0, 0, 1);
     // This creates and positions a free camera
     const camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
     // This targets the camera to scene origin
     camera.setTarget(BABYLON.Vector3.Zero());
     // This attaches the camera to the canvas
     camera.attachControl(canvas, false);
-    // This creates a light, aiming 0,1,0 - to the sky.
-    const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
-    // Dim the light a small amount
-    light.intensity = .5;
-    // Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
-    const sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
-    // Let's try our built-in 'ground' shape. Params: name, width, depth, subdivisions, scene
-    const ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
+
+    // skybox
+    const envTexture = new BABYLON.CubeTexture('/assets/textures/skybox/skybox', scene, this.SKYBOX_EXTENSIONS);
+    scene.createDefaultSkybox(envTexture, true);
+
     // Leave this function
     return scene;
   }
